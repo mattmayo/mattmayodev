c87cfb989ac10df72d49623e183e7e9e96a2c782
Matt Mayo <matt_mayo@icloud.com>
2023-09-24 16:31:28 -0500

    Add prettier create jobs table

--- src/Resume.tsx
+++ src/Resume.tsx
@@ -1,52 +1,59 @@
 import resumeJson from './assets/resume.json'
-import Link from '@mui/material/Link'
-import Timeline from '@mui/lab/Timeline'
-import TimelineItem from '@mui/lab/TimelineItem'
-import TimelineOppositeContent from '@mui/lab/TimelineOppositeContent'
-import TimelineSeparator from '@mui/lab/TimelineSeparator'
-import TimelineConnector from '@mui/lab/TimelineConnector'
-import TimelineContent from '@mui/lab/TimelineContent'
-import TimelineDot from '@mui/lab/TimelineDot'
 import Typography from '@mui/material/Typography'
+import Link from '@mui/material/Link'
+import Table from '@mui/material/Table'
+import TableBody from '@mui/material/TableBody'
+import TableCell from '@mui/material/TableCell'
+import TableContainer from '@mui/material/TableContainer'
+import TableHead from '@mui/material/TableHead'
+import TableRow from '@mui/material/TableRow'
+import dayjs from 'dayjs'
 
 export default function Resume() {
   return (
     <>
       <div>
-        <Typography variant="h4">{resumeJson.basicInfo.name}</Typography>
-        <Link variant="body1" href={resumeJson.basicInfo.linkedIn}>
-          LinkedIn
-        </Link>
-        <br />
-        <Link variant="body1" href={resumeJson.basicInfo.website}>
-          mattmayo.com
-        </Link>
-        <Timeline>
-          <TimelineItem>
-            <TimelineOppositeContent>
-              {resumeJson.jobs[0].startDate} - {resumeJson.jobs[0].endDate}
-            </TimelineOppositeContent>
-            <TimelineSeparator>
-              <TimelineDot />
-              <TimelineConnector />
-            </TimelineSeparator>
-            <TimelineContent>TBD</TimelineContent>
-          </TimelineItem>
-          <TimelineItem>
-            <TimelineSeparator>
-              <TimelineDot />
-              <TimelineConnector />
-            </TimelineSeparator>
-            <TimelineContent>TBD</TimelineContent>
-          </TimelineItem>
-          <TimelineItem>
-            <TimelineSeparator>
-              <TimelineDot />
-            </TimelineSeparator>
-            <TimelineContent>TBD</TimelineContent>
-          </TimelineItem>
-        </Timeline>
+        <Typography variant="h4" align="center">
+          {resumeJson.basicInfo.name}
+        </Typography>
+        <Typography variant="body1" align="center">
+          <Link href={resumeJson.basicInfo.linkedIn}>LinkedIn</Link>
+        </Typography>
+        <Typography variant="body1" align="center">
+          <Link variant="body1" href={resumeJson.basicInfo.website}>
+            mattmayo.com
+          </Link>
+        </Typography>
       </div>
+      <TableContainer>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>Company</TableCell>
+              <TableCell>Role</TableCell>
+              <TableCell>Dates</TableCell>
+              <TableCell>Description</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {resumeJson.jobs.map((job) => (
+              <TableRow key={job.company}>
+                <TableCell>{job.company}</TableCell>
+                <TableCell>{job.role}</TableCell>
+                <TableCell>
+                  {dayjs(job.startDate).format('MMM YYYY')} -{' '}
+                  {dayjs(job.endDate).format('MMM YYYY')}
+                </TableCell>
+                <TableCell>
+                  {job.description.map((p) => (
+                    <p>{p}</p>
+                  ))}
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </>
   )
 }
