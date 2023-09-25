d6d3885a26049d5dfaa1bc60d7ae1b2f2daa5a89
Matt Mayo <matt_mayo@icloud.com>
2023-09-25 13:54:49 -0500

    Add footer to page

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
+import Box from '@mui/material/Box'
+import Link from '@mui/material/Link'
+import Typography from '@mui/material/Typography'
 import Resume from './Resume.tsx'
 
 function App() {
-  return <Resume />
+  return (
+    <>
+      <Resume />
+      <Typography variant="body2">
+        <p>
+          Built with ReactJS and served on NextJS. Code in{' '}
+          <Link href="https://github.com/mattmayo/mattmayodev">Github</Link>
+        </p>
+      </Typography>
+    </>
+  )
 }
 
 export default App
